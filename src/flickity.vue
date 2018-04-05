5e2c0e9fb2073daa7f091f493fdb4d77ab61fa9d
drewjbartlett <drewb9@gmail.com>
2018-04-04 23:25:05 -0500

    Update docs. Reorganize methods to match flickity documentation

--- src/flickity.vue
+++ src/flickity.vue
@@ -17,8 +17,8 @@ export default {
   },
 
   beforeDestroy() {
-    this.flickity.destroy();
-    this.flickity = null;
+    this.$flickity.destroy();
+    this.$flickity = null;
   },
 
   methods: {
@@ -26,38 +26,22 @@ export default {
      * Initialize a new flickity and emit init event.
      */
     init() {
-      this.flickity = new Flickity(this.$el, this.options);
-      this.$emit('init', this.flickity);
+      this.$flickity = new Flickity(this.$el, this.options);
+      this.$emit('init', this.$flickity);
     },
 
     /**
-     * Return flickity data
+     * Return the current flickity instance to access directly
      *
-     * @return {boolean}
+     * @return {Flickity}
      */
-    data() {
-      return Flickity.data(this.$el);
+    flickity() {
+      return this.$flickity;
     },
 
     /**
-     * Change to the next slide
-     *
-     * @param {boolean} isWrapped
-     * @param {boolean} isInstant
+     * Selecting Slides
      */
-    next(isWrapped, isInstant) {
-      this.flickity.next(isWrapped, isInstant);
-    },
-
-    /**
-     * Change to the previous slide
-     *
-     * @param {boolean} isWrapped
-     * @param {boolean} isInstant
-     */
-    previous(isWrapped, isInstant) {
-      this.flickity.previous(isWrapped, isInstant);
-    },
 
     /**
      * Select a slide
@@ -67,7 +51,27 @@ export default {
      * @param {boolean} isInstant
      */
     select(index, isWrapped, isInstant) {
-      this.flickity.select(index, isWrapped, isInstant);
+      this.$flickity.select(index, isWrapped, isInstant);
+    },
+
+    /**
+     * Change to the next slide
+     *
+     * @param {boolean} isWrapped
+     * @param {boolean} isInstant
+     */
+    next(isWrapped, isInstant) {
+      this.$flickity.next(isWrapped, isInstant);
+    },
+
+    /**
+     * Change to the previous slide
+     *
+     * @param {boolean} isWrapped
+     * @param {boolean} isInstant
+     */
+    previous(isWrapped, isInstant) {
+      this.$flickity.previous(isWrapped, isInstant);
     },
 
     /**
@@ -78,58 +82,30 @@ export default {
      * @param {boolean} isInstant
      */
     selectCell(value, isWrapped, isInstant) {
-      this.flickity.selectCell(value, isWrapped, isInstant);
+      this.$flickity.selectCell(value, isWrapped, isInstant);
     },
 
     /**
-     * Return the selected index
-     *
-     * @return {number}
+     * Sizing and Positioning
      */
-    selectedIndex() {
-      return this.flickity.selectedIndex;
-    },
-
-    /**
-     * Return the selected element
-     *
-     * @return {HTMLElement}
-     */
-    selectedElement() {
-      return this.flickity.selectedElement;
-    },
-
-    /**
-     * Return the selected elements
-     *
-     * @return {array}
-     */
-    selectedElements() {
-      return this.flickity.selectedElements;
-    },
-
-    /**
-     * Get the cell elements
-     *
-     * @return {array}
-     */
-    getCellElements() {
-      return this.flickity.getCellElements();
-    },
 
     /**
      * Trigger a resize event
      */
     resize() {
-      this.flickity.resize();
+      this.$flickity.resize();
     },
 
     /**
      * Trigger a reposition event
      */
     reposition() {
-      this.flickity.reposition();
+      this.$flickity.reposition();
     },
+
+    /**
+     * Adding and removing cells
+     */
 
     /**
      * Prepend elements to flickity
@@ -137,7 +113,7 @@ export default {
      * @param {array|HTMLElement|NodeList} elements
      */
     prepend(elements) {
-      this.flickity.prepend(elements);
+      this.$flickity.prepend(elements);
     },
 
     /**
@@ -146,7 +122,7 @@ export default {
      * @param {array|HTMLElement|NodeList} elements
      */
     append(elements) {
-      this.flickity.append(elements);
+      this.$flickity.append(elements);
     },
 
     /**
@@ -156,7 +132,7 @@ export default {
      * @param {number} index
      */
     insert(elements, index) {
-      this.flickity.insert(elements, index);
+      this.$flickity.insert(elements, index);
     },
 
     /**
@@ -165,58 +141,88 @@ export default {
      * @param {array|HTMLElement|NodeList} elements
      */
     remove(elements) {
-      this.flickity.remove(elements);
+      this.$flickity.remove(elements);
     },
+
+    /**
+     * Player
+     */
 
     /**
      * Trigger a playPlayer event
      */
     playPlayer() {
-      this.flickity.playPlayer();
+      this.$flickity.playPlayer();
     },
 
     /**
      * Trigger a stopPlayer event
      */
     stopPlayer() {
-      this.flickity.stopPlayer();
+      this.$flickity.stopPlayer();
     },
 
     /**
      * Trigger a pausePlayer event
      */
     pausePlayer() {
-      this.flickity.pausePlayer();
+      this.$flickity.pausePlayer();
     },
 
     /**
      * Trigger a unpausePlayer event
      */
     unpausePlayer() {
-      this.flickity.unpausePlayer();
+      this.$flickity.unpausePlayer();
     },
 
     /**
      * Trigger a rerender event
      */
     rerender() {
-      this.flickity.destroy();
+      this.$flickity.destroy();
       this.init();
     },
+
+    /**
+     * Utilities
+     */
 
     /**
      * Destroy the flickity instance
      */
     destroy() {
-      this.flickity.destroy();
+      this.$flickity.destroy();
     },
 
     /**
      * Trigger a rerender event
      */
     reloadCells() {
-      this.flickity.reloadCells();
+      this.$flickity.reloadCells();
     },
+
+    /**
+     * Get the cell elements
+     *
+     * @return {array}
+     */
+    getCellElements() {
+      return this.$flickity.getCellElements();
+    },
+
+    /**
+     * Return flickity data
+     *
+     * @return {Flickity}
+     */
+    data() {
+      return Flickity.data(this.$el);
+    },
+
+    /**
+     * Events
+     */
 
     /**
      * Attach an event
@@ -225,7 +231,7 @@ export default {
      * @param {function} listener
      */
     on(eventName, listener) {
-      this.flickity.on(eventName, listener);
+      this.$flickity.on(eventName, listener);
     },
 
     /**
@@ -235,7 +241,7 @@ export default {
      * @param {function} listener
      */
     off(eventName, listener) {
-      this.flickity.off(eventName, listener);
+      this.$flickity.off(eventName, listener);
     },
 
     /**
@@ -245,7 +251,56 @@ export default {
      * @param {function} listener
      */
     once(eventName, listener) {
-      this.flickity.once(eventName, listener);
+      this.$flickity.once(eventName, listener);
+    },
+
+    /**
+     * Properties
+     */
+
+    /**
+     * Return the selected element
+     *
+     * @return {HTMLElement}
+     */
+    selectedElement() {
+      return this.$flickity.selectedElement;
+    },
+
+    /**
+     * Return the selected elements
+     *
+     * @return {array}
+     */
+    selectedElements() {
+      return this.$flickity.selectedElements;
+    },
+
+    /**
+     * Return the selected index
+     *
+     * @return {number}
+     */
+    selectedIndex() {
+      return this.$flickity.selectedIndex;
+    },
+
+    /**
+     * Return the cells
+     *
+     * @return {array}
+     */
+    cells() {
+      return this.$flickity.cells;
+    },
+
+    /**
+     * Return the slides
+     *
+     * @return {array}
+     */
+    slides() {
+      return this.$flickity.slides;
     },
   },
 
